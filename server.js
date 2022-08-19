ea004103444e92b1d2b18942837e2fac316601de
Sean Dillon <pdillon0@gmail.com>
2022-08-18 21:31:17 -0400

    delete button functionality

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 const readFromFile = util.promisify(fs.readFile);
@@ -31,42 +31,64 @@ const writeToFile = (destination, content) =>
 
 
 const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        writeToFile(file, parsedData);
-      }
-    });
-  };
-  
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
 
 app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-  
-    const { title, text, id } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text,
-        id: uuid(),
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(`Note added successfully 🚀`);
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text, id } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+const database = require("./db/db")
+
+app.delete("/api/notes/:id", function (req, res) {
+  let jsonFilePath = path.join(__dirname, "/db/db.json");
+  for (let i = 0; i < database.length; i++) {
+    if (database[i].id == req.params.id) {
+      // Splice takes i position, and then deletes the 1 note.
+      database.splice(i, 1);
+      break;
+    }
+  }
+  fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+
+    if (err) {
+      return console.log(err);
     } else {
-      res.error('Error in adding note');
+      console.log("Your note was deleted!");
     }
   });
+  res.json(database);
+});
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
