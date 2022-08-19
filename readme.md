7863fb95797304595acbccd072bdeb5c0f3e5c6f
Sean Dillon <pdillon0@gmail.com>
2022-08-18 22:23:35 -0400

    readme and comments

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Note-Taker 
+  ## Table of Contents:
+  1. [Description](#description) 
+  2. [Installation](#installation)
+  3. [Usage](#usage)  
+  4. [Contributing](#contribution)
+  5. [Tests](#test)
+  6. [License](#license)
+  7. [GitHub](#github)
+  8. [E-mail](#e-mail)
+## Description
+Simple app for taking and saving notes.  Uses node.js and express and has a fully functional front and backend. Notes are saved via the save button, a note can be discarded and written again with the + button, and deleted with the trash  button.  
+## Installation
+Download the full github repository and run the server using the apps express.js backend on your local machine, or simply use the deployed heroku link. 
+## Usage
+![notes](notetaker.png)
+## Contribution
+Send any push requests you want
+## Test
+Visit the deployed heroku link 
+## License
+This Project is Unlicensed. None
+## GitHub
+https://github.com/DeviousDoge/note_taker
+## E-mail
+pdillon0@gmail.com
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+//Declare dependency variables
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-
+//randomly generate id when called. 
 const uuid = require('./helpers/uuid')
-
+//let the port be heroku deployment or localmachine 3001
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+//define server routes and params. 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,15 +22,15 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
+//declare functions for post requests. 
 const readFromFile = util.promisify(fs.readFile);
-
+//files get read and written in the same function when new note is made.
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
-
+//use for post request when note is made.
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
@@ -42,24 +43,24 @@ const readAndAppend = (content, file) => {
   });
 };
 
-
+//populate the page with notes from db if applicable. 
 app.get('/api/notes', (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
-
+//request to add new notw
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
-
+//identify the note by the content of the front end note object. 
   const { title, text, id } = req.body;
-
+//make new object for back end and add a random id to it. 
   if (req.body) {
     const newNote = {
       title,
       text,
       id: uuid(),
     };
-
+    //add this to db.json
     readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully 🚀`);
   } else {
@@ -68,27 +69,28 @@ app.post('/api/notes', (req, res) => {
 });
 
 const database = require("./db/db")
-
+//delete request for note
 app.delete("/api/notes/:id", function (req, res) {
   let jsonFilePath = path.join(__dirname, "/db/db.json");
+  //loop through the notes by id and find and splice the note that made this request. 
   for (let i = 0; i < database.length; i++) {
     if (database[i].id == req.params.id) {
-      // Splice takes i position, and then deletes the 1 note.
       database.splice(i, 1);
       break;
     }
   }
+  //rewrite the db without the spliced note. 
   fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
 
     if (err) {
       return console.log(err);
     } else {
-      console.log("Your note was deleted!");
+      console.log("Note Deleted!");
     }
   });
   res.json(database);
 });
-
+//listening for hosting from the proper port. 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
